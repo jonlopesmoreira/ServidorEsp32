2e77a5560b6023fb15da92657d53d6a6a233176c
jonlopesmoreira <jonprojetos@hotmail.com>
2026-05-06 03:25:30 -0300

    small fixes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
+const http = require('http');
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ server: http });
+const app = express();
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
 
 let comando = "0";
 
@@ -28,11 +30,11 @@ wss.on('connection', (ws) => {
 app.get('/', (req, res) => {
     res.send(`
     <h1>Controle ESP32</h1>
-    <button onclick="enviar(1)">LIGAR</button>
-    <button onclick="enviar(0)">DESLIGAR</button>
+    <button onclick="enviar('1')">LIGAR</button>
+    <button onclick="enviar('0')">DESLIGAR</button>
 
     <script>
-      let ws = new WebSocket("wss://" + location.host);
+      let ws = new WebSocket((location.protocol === "https:" ? "wss://" : "ws://") + location.host);
 
       function enviar(cmd){
         ws.send(cmd);
@@ -43,6 +45,6 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-http.listen(PORT, () => {
-    console.log("Servidor rodando em http://localhost:3000");
+server.listen(PORT, () => {
+    console.log("Servidor rodando na porta " + PORT);
 });
